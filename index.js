c24958b7fe8a9b3d327862d556374615f4b304b4
Emmanuel Lyimo <esal29t@gmail.com>
2022-01-15 01:33:25 -0800

    modified index.js

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const questions = require('./utils/q')
 // TODO: Create an array of questions for user input
-const questions = [];
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Describe the steps required for installation.",
+            name: 'installation'
+        },
+        {
+            type: 'list',
+            message: "What kind of licence should you project use?",
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'What is your Github user name?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        {
+            type: 'password',
+            message: 'Re-enter password to confirm:',
+            name: 'confirm',
+        },
+    ])
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer  
+      .prompt(questions)
+      .then(answers => {
+        console.log(answers);
+        const myMarkdown = generateMarkdown(answers);
+});
 
+}
 // Function call to initialize app
 init();
